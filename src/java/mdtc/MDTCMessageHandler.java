c32e817c6db68cb5f069192ef9577b84dd0abc6c
Qinjin Wang <qinjin.wang@excosoft.se>
2014-12-27 19:08:51 +0100

    Implemented MDTC message handler. So that Cassandra could running yet another HTTP server as the interface.

---
+++ src/java/mdtc/MDTCMessageHandler.java
@@ -0,0 +1,101 @@
+package mdtc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import mdtc.api.json.GsonHelper;
+import mdtc.api.transaction.data.NodeID;
+import mdtc.api.transaction.data.TItem;
+import mdtc.api.transaction.message.IMessage;
+import mdtc.api.transaction.message.TranEndPointMessage;
+import mdtc.utils.HTTPClientHelper;
+
+import org.apache.cassandra.service.StorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class MDTCMessageHandler implements HttpHandler {
+	private static final Logger logger = LoggerFactory
+			.getLogger(MDTCMessageHandler.class);
+	private static final ExecutorService executorService = Executors
+			.newSingleThreadExecutor();
+
+	@Override
+	public void handle(final HttpExchange exchange) throws IOException {
+
+		executorService.execute(new Runnable() {
+
+			@Override
+			public void run() {
+				InputStream inputStream = exchange.getRequestBody();
+				try {
+					handleMessage(inputStream);
+					exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+				} catch (Exception ex) {
+					logger.error("Exception on handling MDTC messages: {}",
+							ex.getMessage());
+					ex.printStackTrace();
+					try {
+						exchange.sendResponseHeaders(
+								HttpURLConnection.HTTP_INTERNAL_ERROR, -1);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} finally {
+					try {
+						inputStream.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					exchange.close();
+				}
+			}
+		});
+	}
+
+	private void handleMessage(InputStream inputStream) throws Exception {
+		IMessage message = GsonHelper.instance().fromJson(inputStream);
+		logger.info("Handle {} message from {}", message.getMessageType(),
+				message.getSenderID());
+		switch (message.getMessageType()) {
+		case TranEndPoint:
+			onGetEndPoint((TranEndPointMessage) message);
+			break;
+		default:
+			throw new Exception("Not supported message type "
+					+ message.getMessageType());
+		}
+	}
+
+	private void onGetEndPoint(TranEndPointMessage message) {
+		logger.info("Handle {} message from {}", message.getMessageType(),
+				message.getSenderID());
+		for (TItem item : message.items) {
+			List<InetAddress> endPoints = StorageService.instance
+					.getNaturalEndpoints(message.keyspace,
+							message.columnFamily, item.getKey());
+			List<NodeID> endPointIDs = Lists.newArrayList();
+			for (InetAddress endPoint : endPoints) {
+				endPointIDs.add(new NodeID(item.getNodeID().id, endPoint
+						.getHostAddress(), item.getNodeID().port));
+			}
+
+			message.endpoints.put(item.getKey(), endPointIDs);
+		}
+
+		//Send back the endpoint info.
+		NodeID temp = message.sender;
+		message.sender = message.receiver;
+		message.receiver = temp;
+		HTTPClientHelper.getInstance().sendMessage(message);
+	}
+}
--- src/java/mdtc/MDTCServer.java
+++ src/java/mdtc/MDTCServer.java
@@ -1,30 +1,53 @@
 package mdtc;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.cassandra.service.CassandraDaemon.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.net.httpserver.HttpServer;
 
 public class MDTCServer implements Server {
+	private static final Logger logger = LoggerFactory
+			.getLogger(MDTCServer.class);
+
+	private final AtomicBoolean isRunning = new AtomicBoolean(false);
 	private final InetSocketAddress socket;
-	public MDTCServer(InetSocketAddress socket){
+	private HttpServer server;
+
+	public MDTCServer(InetSocketAddress socket) {
 		this.socket = socket;
 	}
+
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
+		try {
+			server = HttpServer.create(socket, 0);
+			server.createContext("/", new MDTCMessageHandler());
+			server.setExecutor(Executors.newCachedThreadPool());
+			server.start();
+			isRunning.set(true);
+			logger.info("MDTCServer is started on {}...", socket);
+		} catch (Exception ex) {
+			logger.error("Exception on start MDTCServer {}", ex.getMessage());
+			ex.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-
+		if (server != null) {
+			server.stop(0);
+			isRunning.set(false);
+		}
 	}
 
 	@Override
 	public boolean isRunning() {
-		// TODO Auto-generated method stub
-		return false;
+		return isRunning.get();
 	}
-
 }
